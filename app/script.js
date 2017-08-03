20529c452b9a90de132c9984a496c71a344fb617
Josiah Keller <josiah@josiahkeller.com>
2017-08-02 23:27:42 -0400

    Support keyboard on clickables

--- app/script.js
+++ app/script.js
@@ -2,12 +2,21 @@ function $(id) {
     return document.getElementById(id);
 }
 
+function clickable(element, handler) {
+    element.addEventListener("click", handler);
+    element.addEventListener("keypress", function(e) {
+        if (e.keyCode !== 13) return;
+        e.preventDefault();
+        handler.call(this, e);
+    });
+}
+
 function toggleSecondaryReasons() {
     document.body.classList.toggle("secondary-reasons-toggled");
 }
 
 function init() {
-    $("secondary-reasons-toggle").addEventListener("click", toggleSecondaryReasons);
+    clickable($("secondary-reasons-toggle"), toggleSecondaryReasons);
 }
 
 document.addEventListener("DOMContentLoaded", init);

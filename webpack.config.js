ec0a744ba9648a65bd15e5d34884fda56286ce33
Josiah Keller <josiahbkeller@gmail.com>
2018-02-15 16:07:46 -0500

    Refactor HTML page content into separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const ejs = require("ejs");
+const homeTemplate = require("./app/views/home.ejs");
 
 module.exports = {
     entry: ["./app/use-adblockers.js", "./app/style.scss"],
@@ -25,9 +29,20 @@ module.exports = {
             filename: "bundle.css",
             allChunks: true,
         }),
+        new HtmlWebpackPlugin({
+            alwaysWriteToDisk: true,
+            title: "Use Adblockers to stay safe online and avoid scams",
+            template: "app/template.ejs",
+            filename: "index.html",
+            extraData: {
+                subtemplate: homeTemplate,
+            },
+        }),
+        new HtmlWebpackHarddiskPlugin(),
     ],
     devServer: {
-        contentBase: path.join(__dirname, "public"),
+        contentBase: [path.join(__dirname, "public/dist"), path.join(__dirname, "public/static")],
+        watchContentBase: true,
         port: 3000,
     },
 };
